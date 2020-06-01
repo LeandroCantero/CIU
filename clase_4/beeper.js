6510ec52885eb973212cbadeb87a99c097797131
LeandroCantero <lcantero18@gmail.com>
2020-06-01 14:58:56 -0300

    Tarea

---
+++ clase_4/beeper.js
@@ -0,0 +1,14 @@
+var boton = document.querySelector("button")
+var p1 = document.getElementById("p1")
+var b = document.querySelector("body")
+/*boton.addEventListener("click", function(){
+    p1.insertAdjacentHTML("beforeend", "<p>BEEP</p>")
+    b.classList.toggle("color")
+})*/
+
+var seEjecutaEvento = function(){
+    p1.insertAdjacentHTML("beforeend", "<p>BEEP</p>")
+    b.classList.toggle("color")
+}
+
+boton.addEventListener("click", seEjecutaEvento)
